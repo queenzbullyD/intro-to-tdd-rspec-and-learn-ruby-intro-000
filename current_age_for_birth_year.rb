ce0d331a31ab2472cfb394c1bd60003e98a7c910
darren a vinson jr <darrenvinson@gmail.com>
2020-11-21 09:31:35 +0000

    Automatically backed up by Learn

--- current_age_for_birth_year.rb
+++
@@ -1,9 +0,0 @@
- require_relative current_age_for_birth_year('1984')
-  puts 'current'
-  current = ('2003')
-  puts 'birth_year'
-  birth_year = ('1984')
-  puts 'age_of_person'
-age_of_person =  (current_year - birth_year)
-
-end
