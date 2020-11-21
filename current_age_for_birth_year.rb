8fc4ae01cfdca59692420615171b29839ac6642d
darren a vinson jr <darrenvinson@gmail.com>
2020-11-21 09:28:18 +0000

    Automatically backed up by Learn

--- current_age_for_birth_year.rb
+++ current_age_for_birth_year.rb
@@ -4,6 +4,6 @@ def current_age_for_birth_year
   puts 'birth_year'
   birth_year = (1984)
   puts 'age_of_person'
-  (current_year - birth_year) = age_of_person
-  
+age_of_person =  (current_year - birth_year)  
+
 end
